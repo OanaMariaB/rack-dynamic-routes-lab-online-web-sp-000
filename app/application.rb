fd498e15126786cb9dbe744c264aa3e7de0b4635
Oana Berentan <oana.berentan@yahoo.com>
2019-10-12 16:52:18 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -12,6 +12,14 @@ class Application
       resp.write "Route not found"
       resp.status = 404
     elsif
+      item = @@items.find{|i| i.name == item_name}
+
+     if item.nil?
+       resp.write "Item not found"
+       resp.status = 400
+     else
+       resp.write item.price
+     end
 
     end
 
