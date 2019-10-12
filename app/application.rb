e14a77b39c177117dc48e2bde7b3e4b62d8e5d3a
Oana Berentan <oana.berentan@yahoo.com>
2019-10-12 16:44:44 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
 
     if req.path == "/items/<ITEM NAME>"
       resp.write "You requested the items"
-    else
+    elsif
       resp.write "Route not found"
       resp.status = 404
     end
